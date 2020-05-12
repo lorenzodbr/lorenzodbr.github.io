7a8db05248f5e79034a358e0973cc27fbff400d2
lorenzodbr <59839252+lorenzodbr@users.noreply.github.com>
2020-05-12 19:44:49 +0200

    Update scrolling-viewer.js

--- nuovi-mondi/js/scrolling-viewer.js
+++ nuovi-mondi/js/scrolling-viewer.js
@@ -3,9 +3,9 @@ var lastScrollTop = 0;
 window.addEventListener("scroll", function(){  
    var st = window.pageYOffset || document.documentElement.scrollTop;  
    if (st > lastScrollTop){
-       document.getElementByClass("bottom-bar").style.bottom = "-100%";
+       document.getElementByClassName("bottom-bar").style.bottom = "-100%";
    } else {
-      document.getElementByClass("bottom-bar").style.bottom = "0";
+      document.getElementByClassName("bottom-bar").style.bottom = "0";
    }
    lastScrollTop = st;
 }, false);
