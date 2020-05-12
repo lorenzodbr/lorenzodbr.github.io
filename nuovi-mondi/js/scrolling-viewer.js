37d493ce075409cb174b927cc876c2d1518be75c
lorenzodbr <59839252+lorenzodbr@users.noreply.github.com>
2020-05-12 18:57:55 +0200

    Update scrolling-viewer.js

--- nuovi-mondi/js/scrolling-viewer.js
+++ nuovi-mondi/js/scrolling-viewer.js
@@ -1,7 +1,7 @@
 var lastScrollTop = 0;
 $(window).scroll(function(event){
    var st = $(this).scrollTop();
-   if (st > lastScrollTop){
+   if (st - 10 > lastScrollTop){
 	   $(".bottom-bar").slideUp(250, "linear", function(){});
    } else {
        $(".bottom-bar").slideDown(250, "linear", function(){});
