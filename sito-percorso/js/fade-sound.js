91f32ca31da354df6a01d37f5ec566a4287b6b76
lorenzodbr <59839252+lorenzodbr@users.noreply.github.com>
2020-05-16 14:14:32 +0200

    update

---
+++ sito-percorso/js/fade-sound.js
@@ -0,0 +1,20 @@
+function getSoundAndFadeAudio (audiosnippetId) {
+
+    var sound = document.getElementById(audiosnippetId);
+
+    // Set the point in playback that fadeout begins. This is for a 2 second fade out.
+    var fadePoint = sound.currentTime; 
+
+    var fadeAudio = setInterval(function () {
+
+        // Only fade if past the fade out point or not at zero already
+        if ((sound.currentTime >= fadePoint) && (sound.volume != 0.0)) {
+            sound.volume -= 0.1;
+        }
+        // When volume at zero stop all the intervalling
+        if (sound.volume === 0.0) {
+            clearInterval(fadeAudio);
+        }
+    }, 200);
+
+}
--- sito-percorso/js/main.js
+++ sito-percorso/js/main.js
@@ -66,10 +66,32 @@ setTimeout(() => {
     page fade
 */
 
+function getSoundAndFadeAudio (audiosnippetId) {
+
+    var sound = document.getElementById(audiosnippetId);
+
+    // Set the point in playback that fadeout begins. This is for a 2 second fade out.
+    var fadePoint = sound.currentTime; 
+
+    var fadeAudio = setInterval(function () {
+
+        // Only fade if past the fade out point or not at zero already
+        if ((sound.currentTime >= fadePoint) && (sound.volume != 0.0)) {
+            sound.volume -= 0.075;
+        }
+        // When volume at zero stop all the intervalling
+        if (sound.volume === 0.0) {
+            clearInterval(fadeAudio);
+        }
+    }, 200);
+
+}
+
 document.querySelectorAll('.js-page-transition').forEach(elem => {
     elem.addEventListener('click', e => {
         e.preventDefault();
-        pageTransition(e.currentTarget.getAttribute('href'))
+        pageTransition(e.currentTarget.getAttribute('href'));
+		getSoundAndFadeAudio('bg-audio');
     })
 })
 
