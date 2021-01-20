37817334c00227b5ddafe4f25cdeddd25c68c3d0
lorenzodbr <59839252+lorenzodbr@users.noreply.github.com>
2021-01-20 16:47:11 +0100

    update

--- prima-rivoluzione-industriale/js/translate.js
+++ prima-rivoluzione-industriale/js/translate.js
@@ -1,4 +1,3 @@
-
 function translate(event) {
 	let testoTradotto = $(event.target).siblings(".testo-tradotto");
 	let testoOriginale = $(event.target).siblings(".testo-originale");
@@ -10,33 +9,25 @@ function translate(event) {
 		linkOriginale = $(event.target);
 		linkTradotto = $(event.target).siblings(".tradotto");
 		
-		testoTradotto.addClass("text-non-active");
-		testoTradotto.removeClass("text-active");
+		testoTradotto.addClass("text-non-active").removeClass("text-active");
 		
-		testoOriginale.removeClass("text-non-active");
-		testoOriginale.addClass("text-active");
+		testoOriginale.removeClass("text-non-active").addClass("text-active");
 		
-		linkOriginale.removeClass("translate-link-non-active");
-		linkOriginale.addClass("translate-link-active");
+		linkOriginale.removeClass("translate-link-non-active").addClass("translate-link-active");
 		
-		linkTradotto.removeClass("translate-link-active");
-		linkTradotto.addClass("translate-link-non-active");
+		linkTradotto.removeClass("translate-link-active").addClass("translate-link-non-active");
 	}
 	else{
 		linkOriginale = $(event.target).siblings(".originale");
 		linkTradotto = $(event.target);
 		
-		testoOriginale.addClass("text-non-active");
-		testoOriginale.removeClass("text-active");
+		testoOriginale.addClass("text-non-active").removeClass("text-active");
 		
-		testoTradotto.removeClass("text-non-active");
-		testoTradotto.addClass("text-active");
+		testoTradotto.removeClass("text-non-active").addClass("text-active");
 		
-		linkTradotto.removeClass("translate-link-non-active");
-		linkTradotto.addClass("translate-link-active");
+		linkTradotto.removeClass("translate-link-non-active").addClass("translate-link-active");
 		
-		linkOriginale.removeClass("translate-link-active");
-		linkOriginale.addClass("translate-link-non-active");
+		linkOriginale.removeClass("translate-link-active").addClass("translate-link-non-active");
 	}
 }
 
